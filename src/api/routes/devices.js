5b8108bf9bd312b4e19374a7c79126bc59f00c05
Alberto Scicali <alberto@Big-Bear-Pro.local>
2020-05-03 15:19:24 -0500

    Removed unused routes under devices

--- src/api/routes/devices.js
+++ src/api/routes/devices.js
@@ -46,6 +46,10 @@ export default (app) => {
    * ~~~~~~~~~~~~~~~~~~~~~
    */
 
+  route.get('/auth/oura', (req, res, next) => {
+    res.redirect(devices.oura.authorizationUri);
+  });
+
   route.get('/auth/oura/callback', async (req, res, next) => {
     console.log('IN CALLBACK');
     const { code } = req.query;
@@ -61,39 +65,6 @@ export default (app) => {
     } catch (error) {
       Logger.warn(error);
       res.redirect(`${config.frontEndUri}/settings`);
-    }
-  });
-
-  route.get('/auth/oura', (req, res, next) => {
-    res.redirect(devices.oura.authorizationUri);
-  });
-
-  route.get('/auth/:device', validate(validationSchemas.paramsDevices), (req, res, next) => {
-    const { device } = req.params;
-    if (!device || !config.devices.includes(device, 0)) {
-      return next(new ErrorHandler(400, `Must include a valid device to authorize: ${config.devices}`));
-    }
-    return res.redirect(devices[device].authorizationUri);
-    // Call to service to get authorizationURI for the correct device
-    // Then redirect to that URI
-  });
-
-  route.get('/auth-callback/oura', async (req, res, next) => {
-    const { code } = req.query;
-    const options = {
-      code,
-    };
-
-    try {
-      const result = await devices.oura.oauth2.authorizationCode.getToken(options);
-
-      console.log(`Resulting token: ${result}`);
-
-      const token = devices.oura.oauth2.accessToken.create(result);
-      // Here you save the access_token, refresh_token, expires_in
-      return res.json(token);
-    } catch (error) {
-      return next(error);
     }
   });
 };
