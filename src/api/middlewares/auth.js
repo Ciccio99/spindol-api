94141d4d601e56b47fb291ac5159b12be1b60e11
Alberto Scicali <alberto@bigbear.local>
2020-05-05 17:54:00 -0500

    Moved to Auth bearer

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -6,11 +6,17 @@ const jwt = require('jsonwebtoken');
 
 const auth = async (req, res, next) => {
   try {
-    // const token = req.header('Authorization').replace('Bearer ', '');
-    const token = req.cookies[config.authCookieName];
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new ErrorHandler(401, 'Please Authenticate');
+    }
+
+    const token = authHeader.split(' ')[1];
+
     if (!token) {
       throw new ErrorHandler(401, 'Please Authenticate');
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
     if (!user) {
@@ -25,34 +31,5 @@ const auth = async (req, res, next) => {
     return next(error);
   }
 };
-
-
-// const auth = async (req, res, next) => {
-//   try {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//       throw new ErrorHandler(401, 'Please Authenticate');
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     if (!token) {
-//       throw new ErrorHandler(401, 'Please Authenticate');
-//     }
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-//     if (!user) {
-//       throw new ErrorHandler(401, 'Please Authenticate');
-//     }
-
-//     req.token = token;
-//     req.user = user;
-
-//     return next();
-//   } catch (error) {
-//     return next(error);
-//   }
-// };
 
 export default auth;
