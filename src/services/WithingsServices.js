ef5424663c71ef2d1023c1c5df485c2f6608b755
Alberto Scicali <alberto@bigbear.local>
2020-06-30 19:21:05 -0500

    testing withings

--- src/services/WithingsServices.js
+++ src/services/WithingsServices.js
@@ -18,14 +18,13 @@ const getSleepSummary = async (user, date) => {
   const dataFields = 'wakeupduration,wakeupcount,snoringepisodecount,snoring,sleep_score,rr_min,rr_max,rr_average,remsleepduration,lightsleepduration,hr_min,hr_max,hr_average,durationtowakeup,durationtosleep,deepsleepduration,breathing_disturbances_intensity'
   const { data } = await axios.get('https://wbsapi.withings.net/v2/sleep', {
     headers: {
-      Authorization: `Bearer ${user.accounts.oura.token.access_token}`,
+      Authorization: `Bearer ${user.accounts.withings.token.access_token}`,
     },
     params: {
       action,
       startdateymd: searchDate,
       enddateymd: searchDate,
       data_fields: dataFields,
-      access_token: user.accounts.oura.token.access_token,
     },
   });
 
